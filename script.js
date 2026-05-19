8307da598f2bf76b0a909665bfc64183f4a9725f
Gustavo <gustavomaragna88@gmail.com>
2026-05-19 16:49:07 -0300

    Second Class JS

---
+++ script.js
@@ -0,0 +1,15 @@
+const nome = "Gustavo Maragna";
+const idade = 21;
+const cidade = "São Paulo";
+const profissao = "Desenvolvedor Fullstack";
+
+console.log(`Nome: ${nome} | Idade: ${idade} anos | Cidade: ${cidade} | Profissão: ${profissao}`);
+
+const num1 = 10;
+const num2 = 5;
+
+console.log(`Soma: ${num1 + num2}`);
+
+const bool = true;
+
+console.log(typeof nome, typeof idade, typeof bool);
